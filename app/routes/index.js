3831dcc02a90d6420fa8db3458f47952d7d93415
HarrisBrandon-FS1 <162524452+HarrisBrandon-FS1@users.noreply.github.com>
2025-01-12 14:14:59 -0800

    lecture 1 setup

---
+++ app/routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+
+`localhost:3000/api/`;
+router.get('/', (req, res) => {
+    res.status(200).json({message: 'From the API'});
+});
+
+`localhost:3000/api/:id`;
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('params >>>', id)
+    res.status(200).json({message: 'From the API'});
+});
+
+
+router.post("/", (req, res) => {
+        console.log('Request body >>>', req.body);
+        const { data } = req.body;
+        res.status(200).json({message: "We got the request", data})
+});
+
+`localhost:3000/api/:id`;
+router.delete('/:id', (req, res) => {
+    const todo = arrayBuffer.find(obj => obj.id == id);
+    const id = req.params.id;
+    console.log('params >>>', id)
+    res.status(200).json({message: 'From the API'});
+});
+
+
+`localhost:3000/api/:id`;
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('params >>>', id)
+    res.status(200).json({message: 'From the API'});
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+require('dotenv').config();
+const app = require('./app/app')
+
+
+http.createServer(app).listen(process.env.PORT, () => {
+    console.log("server is running on PORT", process.env.PORT);
+});
