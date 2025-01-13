8925b8d03aa1c718f083d5754329eb93c1d4a4b2
HarrisBrandon-FS1 <162524452+HarrisBrandon-FS1@users.noreply.github.com>
2025-01-12 16:49:13 -0800

    updating routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,17 +5,17 @@ const todos = [];
 
 `localhost:3000/api/`;
 router.get('/', (req, res) => {
-    res.status(200).json({message: 'From the API'});
+    res.status(200).json({message: 'From the API', todos});
 });
 
 `localhost:3000/api/:id`;
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     console.log('params >>>', id)
-    res.status(200).json({message: 'From the API'});
+    res.status(200).json({message: 'Id found:', id});
 });
 
-
+`localhost:3000/api/`
 router.post("/", (req, res) => {
         console.log('Request body >>>', req.body);
         const { data } = req.body;
@@ -25,10 +25,11 @@ router.post("/", (req, res) => {
 
 `localhost:3000/api/:id`;
 router.delete('/:id', (req, res) => {
-    const todo = arr.find(obj => obj.id == id);
     const id = req.params.id;
+    const todo = todos.find(obj => obj.id == id);
+    todos.splice(id);
     console.log('params >>>', id)
-    res.status(200).json({message: 'From the API'});
+    res.status(200).json({message: "Deleted from server:", id});
 });
 
 
